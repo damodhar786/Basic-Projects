6849e3b8897817ffef58e0a8dee3788e84d4ce72
damodhar <damodharvanjarapu@gmail.com>
2023-03-08 07:52:47 +0530

    BS 7: Digital Clock

--- BS-7/app.js
+++ BS-7/app.js
@@ -1,32 +1,24 @@
 // Fetch Date and Time
 
 let date = new Date();
-console.log(date);
 
 let dayNumber = date.getDay();
-console.log(dayNumber);
 
 let hours = date.getHours();
-console.log(hours);
 
 let minutes = date.getMinutes();
-console.log(minutes);
 
 let ampm = hours >= 12 ? 'PM' : 'AM';
-console.log(ampm);
 
 let monthNumber = date.getMonth();
 
 let dayOfMonth = date.getDate();
-console.log(dayOfMonth)
 
 // Assign To HTML
 
 let hour = document.getElementById("hour");
-hour.innerHTML = hours;
 
 let minute = document.getElementById("minute");
-minute.innerHTML = minutes;
 
 let dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -39,4 +31,16 @@ let month = document.getElementById("month");
 month.innerHTML = monthNames[monthNumber];
 
 let currentDay = document.getElementById("currentDay");
+
+// Hours and Minutes
+
+hours = hours % 12;
+
+hours = hours < 10 ? "0" + hours : hours;
+hour.innerHTML = hours;
+
+minutes = minutes < 10 ? "0" + minutes : minutes;
+minute.innerHTML = minutes;
+
+dayOfMonth = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth;
 currentDay.innerHTML = dayOfMonth;
